870d3de6fabf6c94612bdba6e03ce2545778fea1
Beau De Veau <109639146+hubertbeau@users.noreply.github.com>
2023-03-22 22:27:23 -0400

    Update mysqli-connect.php

--- mysqli-connect.php
+++ mysqli-connect.php
@@ -2,7 +2,7 @@
 include 'config.php';
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$conn = new mysqli('127.0.0.1','root','', 'test');
+$conn = new mysqli('','root','', 'test');
 // Check connection
 if ($conn->connect_error) {
   echo "Connection failed: " . $conn->connect_error;
